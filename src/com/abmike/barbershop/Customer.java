5d368a7757b223d42c039069e9188739e730dbcf
Michael Boadum Ahiale <abmike268@gmail.com>
2024-08-16 23:58:58 +0000

    feat: custer class and customer types

--- src/com/abmike/barbershop/Customer.java
+++ src/com/abmike/barbershop/Customer.java
@@ -1,4 +1,42 @@
 package com.abmike.barbershop;
 
 public class Customer {
+
+    public static final String ORD_PEFIX="ORD";
+    public static final String VIP_PEFIX="VIP";
+
+    private int customerID;
+    private String customerName;
+    private CustomerType customerType;
+
+    public enum CustomerType {
+        ORD, VIP
+    }
+
+    public Customer(int customerID, String customerName, CustomerType customerType) {
+        this.customerID = customerID;
+        this.customerType= customerType;
+        this.customerName = (customerType == CustomerType.VIP? VIP_PEFIX:ORD_PEFIX)+customerID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+
+@Override
+    public String toString() {
+        return customerName;
+}
+
 }
